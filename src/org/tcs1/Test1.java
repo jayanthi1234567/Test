8930ce6099e5e79411cfb85f6ade16155059055d
jayanthi1234567 <jayanthirramesh33@gmail.com>
2023-04-25 12:23:53 +0530

    My task

---
+++ src/org/tcs1/Test1.java
@@ -0,0 +1,16 @@
+package org.tcs1;
+
+public class Test1 {
+	public static void main(String[] args) {
+		
+
+				System.out.println("Exam1");
+				System.out.println("Exam2");
+				System.out.println("Exam3");
+				System.out.println("Exam4");
+				System.out.println("Exam5");
+				System.out.println("Exam6");
+			}
+	}
+
+
